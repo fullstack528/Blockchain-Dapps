ac437a0fdd526a900842fd748a18ccc6ef7e7c33
fullstack528 <108752820+fullstack528@users.noreply.github.com>
2022-08-31 12:51:01 +0900

    puffu

---
+++ Solana/Puffu-staking-contract-main/programs/staking_program/src/constants.rs
@@ -0,0 +1,11 @@
+pub const CLASS_TYPES: usize = 3;
+pub const REWARD_DENOMIATOR: u64 = 10;
+
+pub const GLOBAL_AUTHORITY_SEED: &str = "global-authority";
+pub const RS_PREFIX: &str = "puffu-nft-staking";
+pub const RS_STAKEINFO_SEED: &str = "puffu-stake-info";
+pub const RS_STAKE_SEED: &str = "puffu-nft-staking";
+pub const RS_VAULT_SEED: &str = "puffu-vault";
+
+pub const DAY: i64 = 60 * 60 * 24;
+pub const DECIMAL: u32 = 9;
